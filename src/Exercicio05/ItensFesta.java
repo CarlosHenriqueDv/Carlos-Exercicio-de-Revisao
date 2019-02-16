7264c416fb5833e572a853fdf1ab4c1e95b80781
26148867 <cshenrique02@gmail.com>
2019-02-15 21:30:06 -0300

    exercio 05

---
+++ src/Exercicio05/ItensFesta.java
@@ -0,0 +1,15 @@
+package Exercicio05;
+
+import java.util.Date;
+
+public class ItensFesta {
+
+    private String tipoBuffet;
+    private String tipoSalao;
+    private double valorContrato;
+    private Date dataEvento;
+
+
+
+
+}
